e00c320f6448a8c2b8600de7c1e417462ef31f1c
Michael Palmer <michaelpalmer53@gmail.com>
2026-05-11 18:12:01 +0100

    fixes ordering issue in release workflow

--- fixtures/combined/.benchmark/bundle.js
+++ fixtures/combined/.benchmark/bundle.js
@@ -1,6 +1,6 @@
 /**
  * xGSD bundle.js
- * generated: 2026-05-11T17:00:14.757Z
+ * generated: 2026-05-11T17:11:46.259Z
  * hash: bb0d64108f537465a3221a044030e2694d73b76efc4dd531a162fda5ec45933b
  * WARNING: this file is generated. Do not edit manually.
  */
