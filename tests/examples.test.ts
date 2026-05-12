edfd8fd7e0d1b37577c5bb1212e3307eb95cce84
Michael Palmer <michaelpalmer53@gmail.com>
2026-05-12 19:09:21 +0100

    add test case for quickstart example

--- tests/examples.test.ts
+++ tests/examples.test.ts
@@ -3,14 +3,37 @@ import {createTransport} from '../src/index.js'
 import {createWriteStream, readFileSync, rmSync} from 'fs'
 import {join} from 'path'
 
+const workerPath = join(process.cwd(), 'fixtures', 'combined', 'worker.js')
 describe('doc example tests', () => {
+  test('doc example: Quickstart', async () => {
+    // you'd usually do this during app bootstrap
+    const transport = createTransport({
+      entry: workerPath,
+
+      stream: {
+        write: (chunk: any) => {},
+      },
+    })
+
+    // this would usually be inside a callback
+    // like Express/Koa/etc, really whatever you need
+    // inside a script works perfectly too!
+    const result = await transport({
+      data: {
+        hello: 'world',
+      },
+    })
+
+    expect(result.ok).toBe(true)
+  })
+
   test('doc example: Options', async () => {
     // override default stream
     const stream = createWriteStream('output.jsonl')
 
     const transport = createTransport({
       // entry is the absolute path to your worker.js
-      entry: join(process.cwd(), 'fixtures', 'combined', 'worker.js'),
+      entry: workerPath,
       limits: {
         // time to live in ms
         ttl: 15000,
