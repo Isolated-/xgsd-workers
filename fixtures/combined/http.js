5338e2a57f8ef9514ac87e0657b7a5d7ab512e7a
Michael Palmer <michaelpalmer53@gmail.com>
2026-05-17 21:16:03 +0100

    updates http test to use new hash endpoint

--- fixtures/combined/http.js
+++ fixtures/combined/http.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export default async function worker(data) {
-  const url = 'https://workers-test-api.xgsd.io/json'
+  const url = 'https://workers-test-api.xgsd.io/hash'
 
   const json = (
     await axios.post(url, {
-      convert: {my: 'string'},
+      data: 'hello world',
     })
   ).data
 
